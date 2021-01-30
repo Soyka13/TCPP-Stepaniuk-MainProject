7135d6f7a9762f6f708566b2b23ccaca19856166
Olena Stepaniuk <helen.stepaniuk@gmail.com>
2021-01-30 23:32:00 +0200

    Зміни_2

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,8 +6,8 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello GIT!");
+            Console.WriteLine("------------Hello World!------------");
+            Console.WriteLine("------------Hello GIT!------------");
         }
     }
 }
